e2564a04f2fe0d47fab8aca94de2b38b3e9439f3
Alberto Otero <oteropablos@gmail.com>
2023-03-26 12:42:52 +0200

    test

---
+++ script2.js
@@ -0,0 +1,53 @@
+async function cargarJornadas() {
+    const response = await fetch('jornadas.csv');
+    const data = await response.text();
+    const rows = data.split('\n').slice(1);
+    const container = document.querySelector('.container');
+
+    rows.forEach((row, index) => {
+        const cols = row.split(',');
+
+        if (cols.length > 1) {
+            const jornada = cols[0];
+            const fecha = cols[1];
+            const partido1 = cols[2];
+            const partido2 = cols[3];
+            const partido3 = cols[4];
+            const descanso = cols[5];
+
+            const card = `
+                <div class="card mt-4">
+                    <div class="card-header bg-warning">
+                        <h5 class="text-center">Jornada ${jornada} - ${fecha}</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <p>Partido 1: ${partido1}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p>Partido 2: ${partido2}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p>Partido 3: ${partido3}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p>Descanso: ${descanso}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            container.innerHTML += card;
+        }
+    });
+}
+
+cargarJornadas();
